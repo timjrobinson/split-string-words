ab298ab7afa4d19b7bf6c6a4508a925b034963f5
Ishmael Smyrnow <ismyrnow@appgeo.com>
2015-08-07 15:27:54 -0400

    Add optional word delimiter argument

--- test.js
+++ test.js
@@ -3,8 +3,17 @@
 var split = require("./index");
 var test = require('tap').test;
 
-test('Test', function (t) {
+test('Default behavior', function (t) {
     t.deepEqual(split('hello this is dog'), ["hello", "this", "is", "dog"]);
     t.deepEqual(split('hello "this is dog"'), ["hello", "this is dog"]);
+    t.deepEqual(split('hello.world "this is dog"'), ["hello", "world", "this is dog"]);
+    t.end();
+});
+
+test('Custom delimiter', function (t) {
+    var delimiter = "[^\\s]";
+    t.deepEqual(split('filename hello-world.js', delimiter), ["filename", "hello-world.js"]);
+    t.deepEqual(split('filename hello-world.js', delimiter), ["filename", "hello-world.js"]);
+    t.deepEqual(split('hello.world "this is dog"', delimiter), ["hello.world", "this is dog"]);
     t.end();
 });
